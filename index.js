e2aa6354bed9e9fc6a9a022ef2edcda91cda01f1
naga361111 <naga3611@gmail.com>
2023-10-21 14:09:54 +0900

    bot activity updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 
 
 // Create a new client instance
@@ -8,6 +8,12 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
+
+	client.user.setPresence({
+		activities: [{ name: `Seongsu High School`, type: ActivityType.Playing }],
+		status: 'dnd',
+	});
+
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
